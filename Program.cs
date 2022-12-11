5c7d68bf1e9990418fc84a81604e86cefc9b3b34
doston <rikhsiboevdoston@email.com>
2022-12-11 11:37:07 +0500

    hello

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int Min(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 < result) result = arg2;
+    if (arg3 < result) result = arg3;
+    return result;
+    
+}
+//               0   1   2   3   4   5   6   7   8
+int[] array = { 14, 21, 31, 10, 15, 61, 18, 16, 19 };
+
+int result = Min(
+    Min(array[0], array[1], array[2]),
+    Min(array[3], array[4], array[5]),
+    Min(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
